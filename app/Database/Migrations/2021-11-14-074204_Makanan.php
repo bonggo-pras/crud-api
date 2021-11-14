679bf67974ae7c80a04c0bc910e6107e0a14c57d
Bonggo Prasetyanto <userpras@gmail.com>
2021-11-14 14:53:05 +0700

    menambahkan migrasi tabel dan update README

---
+++ app/Database/Migrations/2021-11-14-074204_Makanan.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Makanan extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '255',
+            ],
+            'jumlah'       => [
+                'type'           => 'INT',
+                'constraint'     => '11',
+            ],
+            'berat' => [
+                'type'           => 'INT',
+                'constraint'     => '11',
+            ],
+            'jumlah' => [
+                'type'           => 'INT',
+                'constraint'     => '11',
+            ]
+        ]);
+        
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('tbl_makanan');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('makanan');
+    }
+}
